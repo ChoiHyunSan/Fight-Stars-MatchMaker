dfd73563867c77f8d15781087fb92ad1cb26a31e
ChoiHyunSan <chsan626@gmail.com>
2025-07-10 20:09:05 +0900

    [CHORE] ENV 파일을 통해 Config 값 보관

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,9 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+    // env
+    implementation("io.github.cdimascio:dotenv-java:3.0.0")
 }
 
 tasks.test {
--- src/main/java/fightStars/matchmaker/config/MatchMakerConfig.java
+++ src/main/java/fightStars/matchmaker/config/MatchMakerConfig.java
@@ -1,10 +1,16 @@
 package fightStars.matchmaker.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class MatchMakerConfig {
-	public static final String MODE = System.getenv().getOrDefault("MM_MODE", "Deathmatch");
-	public static final int TEAM_SIZE = Integer.parseInt(System.getenv().getOrDefault("MM_TEAM_SIZE", "1"));
-	public static final int WORKER_COUNT = Integer.parseInt(System.getenv().getOrDefault("MM_WORKERS", "1"));
-	public static final String JWT_SECRET = System.getenv().getOrDefault("MM_JWT_SECRET", "qwlkfduiwqhrfkeunfsdukfasfzjbcxffeqkufhqerkuhdkfzushfasdfsfqetqet");
-	public static final int EXPIRE_MILL = System.getenv().getOrDefault("MM_EXPIRE_MILL", "300000").equals("0") ? 0 : Integer.parseInt(System.getenv().getOrDefault("MM_EXPIRE_MILL", "300000"));
+
+	public static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+	public static final String MODE = dotenv.get("MM_MODE", "Deathmatch");
+	public static final int TEAM_SIZE = Integer.parseInt(dotenv.get("MM_TEAM_SIZE", "1"));
+	public static final int WORKER_COUNT = Integer.parseInt(dotenv.get("MM_WORKERS", "1"));
+	public static final String JWT_SECRET = dotenv.get("MM_JWT_SECRET", "default_secret");
+	public static final int EXPIRE_MILL = Integer.parseInt(dotenv.get("MM_EXPIRE_MILL", "300000"));
+	public static final String REDIS_URL = dotenv.get("REDIS_URL", "redis://localhost:6379");
+	public static final String ROOM_API = dotenv.get("ROOM_API", "http://localhost:8080/room");
 }
 
